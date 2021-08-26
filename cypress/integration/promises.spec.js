0ae56b8f936f2394e5d1e4033967d80293d5f904
roberta lacerda <robertalacerda@aix.com.br>
2021-08-26 10:54:47 -0300

    Adição de atualização no projeto

---
+++ cypress/integration/promises.spec.js
@@ -0,0 +1,65 @@
+
+
+/*it('Sem testes, ainda', () => {})
+
+const getSomething = (callback) => {
+    setTimeout(() =>{
+        callback (12);
+    }, 1000)
+}
+
+const system = () => {
+    console.log('init');
+
+    getSomething(some => {
+        console.log(`Something is ${some}`)
+        console.log('end');
+    })
+}
+
+system();
+*/
+
+/*it('sem testes, ainda', () => {})
+
+const getSomething = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(13);
+        }, 1000)
+    })
+
+}
+
+const system = () => {
+    console.log('init');
+    getSomething().then(some => {
+    
+        console.log(`Something is ${some}`)
+    })
+    console.log('end')
+}
+
+system();*/
+
+it('sem testes, ainda', () => {})
+
+const getSomething = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(13);
+        }, 1000)
+    })
+
+}
+
+const system = async () => {
+    console.log('init');
+    const some = await getSomething()
+        console.log(`Something is ${some}`)
+    
+    console.log('end')
+}
+
+system();
+
