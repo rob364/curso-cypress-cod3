f3b3952d1f592d18f2a7927680a21d4dd101b2cf
roberta lacerda <robertalacerda@aix.com.br>
2021-08-27 10:22:31 -0300

    subindo terceira aula atualizando o projeto

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+it.only('A external test...', () => {
+
+})
+
+describe('Should group tests...', () => {
+    describe('Should group more specific tests...', () => {
+        it('A specific test...', () => {
+
+    })
+
+})
+
+describe('Should group more specific tests...', () => {
+    it('A specific test 2...', () => {
+
+})
+
+})
+
+    it('A internal test...', () => {
+
+    })
+})
