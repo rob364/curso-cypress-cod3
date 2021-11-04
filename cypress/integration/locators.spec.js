21818b5dbb17f36518fdcfadac5aa9904bcf95f4
roberta lacerda <robertalacerda@aix.com.br>
2021-11-04 12:37:57 -0300

    atualização de projeto

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Work with basic elements', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+    it('...', () => {
+
+    })
+})
