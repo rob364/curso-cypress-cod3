04c90b9c5253400538fa951db4530a43790ab233
roberta lacerda <robertalacerda@aix.com.br>
2021-11-05 14:07:08 -0300

    Atualização do projeto cypress

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -8,7 +8,11 @@ describe('Work with basic elements', () => {
     beforeEach(() => {
         cy.reload()
     })
-    it('...', () => {
-
+    it.only('using jquery selector', () => {
+        cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
+        cy.get('table#tabelaUsuarios tbody > tr:eq(0) td:nth-child(3) > input')
+        cy.get("[onclick*='Francisco']")
+        cy.get('#tabelaUsuarios td:contains(\'Doutorado\'):eq(0) ~ td:eq(3) > input')
+        cy.get('#tabelaUsuarios tr:contains(\'Doutorado\'):eq(0) td:eq(6) input')
     })
 })
