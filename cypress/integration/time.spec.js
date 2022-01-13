3b51e46d2f27531174d1549ee471d401abf9e126
roberta lacerda <robertalacerda@aix.com.br>
2022-01-13 13:21:52 -0300

    Atualização

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress"/>s
+
 describe('Work with alerts', () => {
     before(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
@@ -14,5 +16,21 @@ describe('Work with alerts', () => {
         cy.clock(dt.getTime())
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain','10/04/2012')
+    })
+    it.only('Goes to the future', ()=> {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain','16420')
+        cy.get('#resultado > span').invoke('text').should('gt', 1642079669102)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+        cy.wait(1000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 5000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 5000)
     })
 })
